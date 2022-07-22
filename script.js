7c849e1cf5e7610ec83a448b66768bb458adf521
Christopher Nguyen <christopher714@gmail.com>
2022-07-21 22:43:29 -0700

    Finish grid and grid creation
        Added functions to remove and create new grids when using
        button to create a new grid.
    
        Revised existing function to give a unique id to each row and
        column
    
        Gave body and html width and height specifications and removed
        margin

---
+++ script.js
@@ -0,0 +1,48 @@
+const whole = document.querySelector('#whole');
+
+function creategrid(columns,rows) {
+    for (let columnstart=1;columnstart<=columns;++columnstart) {
+        createcolumn(columnstart);
+
+        for (let rowstart=1;rowstart<=rows;++rowstart) {
+            createrow(columnstart, rowstart)
+        }
+    }
+    
+};
+
+function createrow(columnid,rowi) {
+    const columni = document.querySelector(`#column${columnid}`);
+    const row = document.createElement('div');
+    row.setAttribute('id', `row${rowi}`)
+    row.classList.add('row');
+    // row.textContent='x';
+    columni.appendChild(row);
+}
+
+function createcolumn(columni) {
+    const column = document.createElement('div');
+    column.setAttribute('id', `column${columni}`);
+    column.classList.add('column');
+    // column.textContent='beepboop';
+    whole.appendChild(column);
+}
+
+creategrid(16,16);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    let squares=prompt('How many squares on each side of the grid? (100 max)',
+     16);
+    if (squares != null && squares<=100) {
+        removeAllChildNodes(whole)
+        creategrid(squares, squares);
+    };
+
+})
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
